c1996e8d0c027415bc4af88c074ba51740ce992b
Bacherikov <bacherikovn@gmail.com>
2024-11-11 02:57:25 +0200

    add auth window

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "edu.pet.tradeapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "edu.pet.tradeapp"
--- app/src/main/java/edu/pet/tradeapp/AuthActivity.kt
+++ app/src/main/java/edu/pet/tradeapp/AuthActivity.kt
@@ -1,28 +1,20 @@
 package edu.pet.tradeapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val helloWorldText = findViewById<TextView>(R.id.hello_world)
-        helloWorldText!!.setOnClickListener {
-            val intent: Intent = Intent(this, AuthActivity::class.java)
-            startActivity(intent)
         }
     }
 }
